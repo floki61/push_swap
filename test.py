5876f0024392f2de0edb88f0b956572574c820ac
FLOKI <93949738+floki61@users.noreply.github.com>
2022-03-18 00:27:25 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,4 +3,3 @@ arg = int(input())
 stack_a = []
 stack_a = sample(range(arg), arg)
 print(' '.join([str(i) for i in stack_a]))
-# make re ; make bonus ;ARG=$(python3 ./test.py); ./push_swap $ARG  | wc -l  
